11e1028d6af7a98ee47c3cf64f66d0b80a1d9287
Katja Pönigk <156422876+katjapoenigk@users.noreply.github.com>
2024-01-14 17:46:32 +0100

    Create top10_rentalrevenue_by_country.sql

---
+++ 03 Scripts/top10_rentalrevenue_by_country.sql
@@ -0,0 +1,11 @@
+ --top 10 (rental) revenue by country
+SELECT SUM (A.amount), F.country
+FROM payment A
+INNER JOIN rental B ON A.rental_id = B.rental_id
+INNER JOIN customer C ON B.customer_id = C.customer_id
+INNER JOIN address D ON C.address_id = D.address_id
+INNER JOIN city E ON D.city_id = E.city_id
+INNER JOIN country F ON E.country_id = F.country_id
+GROUP BY F.country
+ORDER BY sum DESC
+LIMIT 10
